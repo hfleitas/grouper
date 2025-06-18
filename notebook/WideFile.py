a96abebd957409b62a122f1fc1b4acf42c602703
AashikMadhavaraoMicrosoft <122925697+AashikMadhavaraoMicrosoft@users.noreply.github.com>
2025-06-18 11:06:24 +0530

    Update WideFile.py

--- notebook/WideFile.py
+++ notebook/WideFile.py
@@ -10,13 +10,25 @@
 # In[6]:
 
 
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import to_json, struct
-import pandas as pd
-df = pd.read_parquet("https://rtainaday.blob.core.windows.net/sample/20250401_162451_44_51.parquet?sp=r&st=2025-04-03T19:39:37Z&se=2029-04-04T03:39:37Z&spr=https&sv=2024-11-04&sr=b&sig=BIUY%2F4hJpVGhBpUt6QrqeJX3zWIw24Pq1axHJd44R6U%3D")
+# from pyspark.sql import SparkSession
+# from pyspark.sql.functions import to_json, struct
+# import pandas as pd
+# df = pd.read_parquet("https://rtainaday.blob.core.windows.net/sample/20250401_162451_44_51.parquet?sp=r&st=2025-04-03T19:39:37Z&se=2029-04-04T03:39:37Z&spr=https&sv=2024-11-04&sr=b&sig=BIUY%2F4hJpVGhBpUt6QrqeJX3zWIw24Pq1axHJd44R6U%3D")
 
-dataFrame = spark.createDataFrame(df)
-jsonDf = dataFrame.select(to_json(struct("*")).alias("data"))
+# dataFrame = spark.createDataFrame(df)
+# jsonDf = dataFrame.select(to_json(struct("*")).alias("data"))
+
+from pyspark.sql.functions import to_json, struct , date_format
+import multiprocessing
+
+path = f"abfss://widefilenewfile@iotmonitoringsa26915.dfs.core.windows.net/2025-06-11_09_01_07.parquet"
+df = spark.read.parquet(path) 
+num_partitions = multiprocessing.cpu_count() * 3
+df = df.repartition(num_partitions)                    
+
+# formatted_df = df.withColumn("Time", date_format("Time", "yyyy-MM-dd HH:mm:ss.SSSSSS"))
+jsonDf = df.select(to_json(struct("*")).alias("data"))
+jsonDf = jsonDf.persist()
 
 
 # #### Write to Eventhouse with JSON mapping
